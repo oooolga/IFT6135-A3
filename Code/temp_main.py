cf0d736dcfb2570cb852feffdcd8a516ba07a820
oooolga <olga.xu823@gmail.com>
2018-03-24 18:05:07 -0400

    commit memory class

---
+++ Code/temp_main.py
@@ -0,0 +1,19 @@
+from util import *
+from memoryNTM import *
+
+if __name__ == '__main__':
+	N = 10
+	M = 15
+	batch_size = 30
+
+	temp = NTMMemory(N, M, batch_size)
+
+	w_t = Variable(torch.Tensor(batch_size, N))
+	e_t = Variable(torch.Tensor(batch_size, M))
+	a_t = Variable(torch.Tensor(batch_size, M))
+	k_t = Variable(torch.Tensor(batch_size, M))
+	beta_t = Variable(torch.Tensor(batch_size))
+
+	temp.reading(w_t)
+	temp.writing(w_t, e_t, a_t)
+	temp.content_addressing(k_t, beta_t)
