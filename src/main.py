c381a9fd4b295288281cf2deaac49f57614eddf3
Yuchen Lu <luyuchen.paul@gmail.com>
2018-03-26 01:43:21 -0400

    done with training loop. try to fit the data first

--- src/main.py
+++ src/main.py
@@ -10,22 +10,29 @@ import argparse
 import utils
 import numpy as np
 
+
 parser = argparse.ArgumentParser("NTM Copy Task")
 parser.add_argument("--model", default="lstm_ntm",
                     help="[baseline] | [lstm_ntm] | [mlp_ntm] ")
-parser.add_argument("--batch-size", default=2)
-parser.add_argument("--train-steps", default=1000,
+parser.add_argument("--batch-size", default=10)
+parser.add_argument("--train-steps", default=50,
                     help="number of steps to train")
-parser.add_argument("--test-interval", default=20)
-parser.add_argument("--lr", default=1e-4)
+parser.add_argument("--print-freq", default=5)
+parser.add_argument("--lr", default=1e-5)
+parser.add_argument("--clip", default=0.25, type=float,
+                    help="gradient clipping")
+
 parser.add_argument("--M", default=20)
 parser.add_argument("--N", default=128)
 parser.add_argument("--controller-size", default=100)
 
 parser.add_argument("--seq-dim", default=8, help="copy task input dim")
 parser.add_argument("--max-seq-len", default=20, help="copy task input length")
+
+parser.add_argument("--logdir", default="./logs", type=str, help="output log for plots")
 args = parser.parse_args()
 use_cuda = torch.cuda.is_available()
+##########################END OF ARGS###############################
 
 # Copy Task
 copy_task_gen = CopyTaskGenerator(
@@ -67,22 +74,27 @@ optimizer = optim.RMSprop(
     alpha=0.95, lr=args.lr
 )
 
+criterion = torch.nn.BCELoss()
 global_step = 0
+loss_avg = 0
+writer = utils.Logger(args.logdir)
+
 while global_step < args.train_steps:
     inp, target = copy_task_gen.generate_batch(batch_size=args.batch_size)
-
-    ipdb.set_trace()
     pred = model(inp)
 
+    optimizer.zero_grad()
+    loss = criterion(pred, target)
+    loss_avg += loss.data[0]
+    loss.backward()
+
+    torch.nn.utils.clip_grad_norm(model.parameters(), args.clip)
+    optimizer.step()
+    global_step += 1
+
+    if global_step % args.print_freq == 0:
+        print("at step {} loss {}".format(global_step, loss_avg / args.print_freq))
+        writer.scalar_summary("train_bce", loss_avg, global_step)
+        loss_avg = 0
 
 
-#for i in range(200):
-#    optimizer.zero_grad()
-#    y_out = baseline_model(inp)
-#
-#    loss = baseline_model.loss(y_out, target)
-#    loss.backward()
-#    optimizer.step()
-#
-#    if i % 20:
-#        print(float(loss.data))
