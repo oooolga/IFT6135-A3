103dd5a64e8d5f8f1b4fb38ff38121b602cf8dfb
Yuchen Lu <luyuchen@elisa1.iro.umontreal.ca>
2018-03-28 18:20:53 -0400

    add env

--- src/slurm_yuchen.sh
+++ src/slurm_yuchen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PYTHONUNBUFFERED=1
 PYTHON_BIN="/u/luyuchen/miniconda2/envs/pytorch3/bin/python"
-$PYTHON_BIN main.py --logdir debug
+$PYTHON_BIN main.py --model lstm_ntm --logdir lstmntm_logs
